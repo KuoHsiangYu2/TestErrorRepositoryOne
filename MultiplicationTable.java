e320b0d2dc4409e24fa193eeb37e35328e1dd1a1
KuoHsiangYu2 <hsiangyugithub@gmail.com>
2022-09-11 23:01:55 +0800

    multiplication-table add 1-for-loop code

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -2,5 +2,14 @@ import static java.lang.System.out;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        /* 單層for迴圈 */
+        for (int c = 1; c <= 81; ++c) {
+            int i = ((c - 1) / 9) + 1;
+            int j = ((c - 1) % 9) + 1;
+            out.printf("%d×%d=%02d ", j, i, (j * i));
+            if (j == 9) {
+                out.println();
+            }
+        }
     }
 }
