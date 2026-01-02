40c212c36811b3568bd3c9631183ea9cff05c677
KuoHsiangYu2 <hsiangyugithub@gmail.com>
2026-01-02 09:16:58 +0800

    commit 09

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        /* 傳統雙層for迴圈 */
+        /* 傳統雙層for迴圈___ */
         for (int i = 1; i <= 9; ++i) {
             for (int j = 1; j <= 9; ++j) {
                 out.printf("%d×%d=%02d ", j, i, (j * i));
